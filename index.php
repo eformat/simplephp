31551a1b27fced195a40b5145d34a6071397e7a6
mike hepburn <eformat@gmail.com>
2015-10-30 13:35:25 +1300

    Update index.php
    updated

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
 
-$IP = isset($_SERVER['SERVER_ADDR'])?$_SERVER['SERVER_ADDR']:gethostbyname(gethostname());
-echo "The All Blacks will win the world cup ! - ServerIP: ", $IP, "\n";
+echo "The All Blacks will win the world cup ! \n";
 
